9766d98f586e28fb9346a0d4f5c04ada850ba84d
Lucas Fogliarini Pedroso <lucas.pedroso@cogna.com.br>
2024-01-21 10:26:06 -0300

    Add project files.

---
+++ Repository.AzureTables.Tests/Account.cs
@@ -0,0 +1,15 @@
+﻿using Azure;
+using Repository.AzureTables;
+
+namespace Repository.Tests
+{
+	public class Account : Entity
+	{
+		public string? RowKey { get; set; }
+		public DateTimeOffset? Timestamp { get; set; }
+		public string? Email { get; set; }
+        public int Age { get; set; }
+        public string? PartitionKey { get; set; }
+		public ETag ETag { get; set; }
+	}
+}
---
+++ Repository.AzureTables.Tests/AzureTablesRepositoryTests.cs
@@ -0,0 +1,54 @@
+using Microsoft.Extensions.DependencyInjection;
+using Repository.AzureTables;
+
+namespace Repository.Tests
+{
+	public class AzureTablesRepositoryTests : RepositoryTests
+	{
+		protected override void AddServices(ServiceCollection serviceCollection)
+		{
+			var storageConnectionString = "";
+			serviceCollection.AddAzureTablesRepository(storageConnectionString);
+		}
+
+		[Fact]
+		public void Where()
+		{
+			var repository = ServiceProvider.GetService<IRepository>();
+			var accounts = repository.Where<Account>(e => e.Email == "lucasfogliarini@gmail.com" && e.Age >= 0);
+		}
+
+		[Fact]
+		public async Task CommitAsync()
+		{
+			var repository = ServiceProvider.GetService<IRepository>()!;
+
+			var count = repository.Where<Account>(e => e.Email == "lucasfogliarini@gmail.com" || e.Email == "luanabueno@gmail.com").Count();
+			if (count < 2)
+			{
+				var accountEntities = new List<Account>
+				{
+					new() {
+						Email = "lucasfogliarini@gmail.com",
+						Age = 33
+					},
+					new() {
+						Email = "luanabueno@gmail.com",
+						Age = 30
+					}
+				};
+				repository.UpdateRange(accountEntities);
+				await repository.CommitAsync();
+			}
+			
+			var lucasAccount = repository.Where<Account>(e => e.Email == "lucasfogliarini@gmail.com").FirstOrDefault();
+			var luanaAccount = repository.Where<Account>(e => e.Email == "luanabueno@gmail.com").FirstOrDefault();
+
+			repository.Remove(lucasAccount);
+
+			luanaAccount.Age = 60;
+			repository.Update(luanaAccount);
+			await repository.CommitAsync();
+		}
+	}
+}
